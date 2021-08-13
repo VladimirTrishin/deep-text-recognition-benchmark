234cd2b86d0034464baa02b91eac8f3e6e3496ac
Vladimir Trishin <v.trishin@inforion.ru>
2021-08-13 13:30:53 +0300

    add albumentations for augmentations

--- dataset/bankcard_dataset.py
+++ dataset/bankcard_dataset.py
@@ -4,7 +4,8 @@ import numpy as np
 from PIL import Image
 from imgaug import augmenters as iaa
 from torch.utils.data.dataset import Dataset
-
+import albumentations as aug
+import cv2
 class Augmentation(object):
     def __init__(self) -> None:
         self.seq = iaa.Sequential([
@@ -26,9 +27,33 @@ class Augmentation(object):
     def __call__(self, image):
         return self.seq(image=image) 
 
+class Augmentation2(object):
+    def __init__(self) -> None:
+        self.seq = aug.Compose([
+            aug.Emboss(),
+            aug.Sharpen(),
+            aug.GaussianBlur(sigma_limit=(0, 1.0)),
+            aug.GaussNoise(),
+            aug.RandomShadow(),
+            aug.RandomSunFlare(src_radius=80),
+            aug.RandomBrightnessContrast(brightness_limit=(-0.5, 0.5), contrast_limit=(-0.5, 0.5)),
+            aug.Downscale(scale_min=0.4, scale_max=0.8),
+            aug.Affine(
+                rotate=(-3, 3),
+                fit_output=True,
+                scale={"x": (0.5, 1.0), "y": (0.5, 1.0)},
+                translate_percent={"x": (-0.04, 0.04), "y": (-0.06, 0.06)}
+                ),
+            aug.Perspective(scale=(0, 0.03), fit_output=True),
+            aug.RandomScale(scale_limit=(0.3, 1.0), interpolation=cv2.INTER_NEAREST)
+        ])
+
+    def __call__(self, image):
+        return self.seq(image=image)['image']
+
 class BankCardDataset(Dataset):
     
-    def __init__(self, root, min_size=4, max_size=5, augmentation=Augmentation(), rgb=False):
+    def __init__(self, root, min_size=4, max_size=5, augmentation=Augmentation2(), rgb=False):
         self.color_format = 'RBG' if rgb else 'L'
         self.augmentation = augmentation
         self.min_size = min_size
